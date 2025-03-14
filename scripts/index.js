413686f5172299ae770f4b5680c50e2aec1e3a74
Jae Kyeong Park <jaekyeongpark@Jaes-MacBook-Pro.local>
2025-03-14 16:29:55 +0900

    fix: html main contents

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,10 @@
+window.addEventListener("scroll", function () {
+  const header = document.getElementById("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
 
 onDOMContentLoaded = (event) => {};
