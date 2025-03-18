529e5c3ed1b2e6e9b89798cd19da99d101b8bd41
Shin-Yu-1 <shingyu.user@gmail.com>
2025-03-18 11:50:51 +0900

    add: 세팅 완료

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+};
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    hot: true,
+    historyApiFallback: true,
+  },
+};
