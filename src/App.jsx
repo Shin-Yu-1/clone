2f018c5ed6e29463183730f21790835e7cb226c1
Shin-Yu-1 <shingyu.user@gmail.com>
2025-03-25 13:22:01 +0900

    fix: import 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from '@/router/index';
-import { ThemeProviderWrapper } from '@/store/ThemeContext.js';
 import GlobalStyle from '@/styles/globalStyle.js';
 
 function App() {
