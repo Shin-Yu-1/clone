d9a2c6aaf62439e911353242b68a80c59b44eb45
Shin-Yu-1 <shingyu.user@gmail.com>
2025-03-31 18:24:40 +0900

    fix: layout metadata

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
+  experimental: {
+    typedRoutes: true,
+  },
 };
 
 export default nextConfig;
