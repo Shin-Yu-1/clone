4db677ef8afa5f09f489a5c0a3f65b55800db909
Shin-Yu-1 <shingyu.user@gmail.com>
2025-04-16 10:41:31 +0900

    :sparkles: feat: story에 보여질 사용자 이름 추가

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,5 +1,29 @@
+import { storyMockData } from '../data/storyMockData.js';
+
 const Story = (storyLayout) => {
-  console.log(storyLayout);
+  const stories = document.createElement('ul');
+  stories.className = 'stories';
+
+  storyLayout.appendChild(stories);
+
+  storyMockData.forEach((item) => {
+    const story = document.createElement('li');
+    story.className = `story-layout`;
+
+    const profile = document.createElement('div');
+    profile.className = 'profile';
+
+    const username = document.createElement('div');
+    username.className = 'username';
+
+    story.appendChild(profile);
+    story.appendChild(username);
+    stories.appendChild(story);
+
+    const name = document.createElement('span');
+    name.textContent = item.username;
+    username.appendChild(name);
+  });
 };
 
 export default Story;
