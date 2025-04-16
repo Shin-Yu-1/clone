6f6440596d0ada789ca6ad4edda2c0dedc10426d
Shin-Yu-1 <shingyu.user@gmail.com>
2025-04-16 22:08:34 +0900

    :sparkles: feat: 스토리 요소 추가

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,17 +1,19 @@
 import { storyMockData } from '../data/storyMockData.js';
+import { paging } from '../util/paging.js';
 
 const Story = (storyLayout) => {
   const stories = document.createElement('ul');
   stories.className = 'stories';
 
   storyLayout.appendChild(stories);
+  const storiesData = paging({ arr: storyMockData, size: 8, page: 1 });
 
-  storyMockData.forEach((item) => {
+  storiesData.forEach((item) => {
     const story = document.createElement('li');
-    story.className = `story-layout`;
+    story.className = `story-wrapper`;
 
     const profile = document.createElement('div');
-    profile.className = 'profile';
+    profile.className = 'profile-wrapper';
 
     const username = document.createElement('div');
     username.className = 'username';
@@ -20,7 +22,17 @@ const Story = (storyLayout) => {
     story.appendChild(username);
     stories.appendChild(story);
 
+    const storyRing = document.createElement('div');
+    storyRing.className = 'story-ring';
+    profile.appendChild(storyRing);
+
+    const img = document.createElement('img');
+    img.src = item.profileImage;
+    img.className = 'story-profile';
+    storyRing.appendChild(img);
+
     const name = document.createElement('span');
+    name.className = 'name';
     name.textContent = item.username;
     username.appendChild(name);
   });
