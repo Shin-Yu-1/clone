847d904e485eec812e78ba4bd61b0fdbd98f025b
Shin-Yu-1 <shingyu.user@gmail.com>
2025-04-23 17:04:59 +0900

    refactor: api type 수정함

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,21 +1,33 @@
-export interface MediaItem {
-  adult: boolean;
-  backdrop_path: string | null;
-  genre_ids: number[];
+export type MediaType = 'movie' | 'tv';
+
+export interface BaseMedia {
   id: number;
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
   original_language: string;
   overview: string;
   popularity: number;
-  poster_path: string | null;
+  poster_path: string;
   vote_average: number;
   vote_count: number;
-  name?: string;
-  original_name?: string;
-  first_air_date?: string;
-  origin_country?: string[];
-  title?: string;
-  original_title?: string;
-  release_date?: string;
-  video?: boolean;
-  media_type?: 'movie' | 'tv' | 'person';
+  media_type: MediaType;
 }
+
+export interface MovieItem extends BaseMedia {
+  media_type: 'movie';
+  title: string;
+  original_title: string;
+  release_date: string;
+  video: boolean;
+}
+
+export interface TvItem extends BaseMedia {
+  media_type: 'tv';
+  name: string;
+  original_name: string;
+  first_air_date: string;
+  origin_country: string[];
+}
+
+export type MediaItem = MovieItem | TvItem;
