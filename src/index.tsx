ef6f47b8e1b0d9cace6dad8beef8b5d5a6b7b606
Shin-Yu-1 <shingyu.user@gmail.com>
2025-04-23 14:49:34 +0900

    refactor: jsx에서 tsx로 변경

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from '@/App.jsx';
+
+import '@/styles/reset.css';
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) throw new Error('Failed to find the root element');
+
+ReactDOM.createRoot(rootElement).render(
+  <>
+    <App />
+  </>
+);
