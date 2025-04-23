20e60eb30797afbd5112201b2b9d39aee6072d23
Shin-Yu-1 <shingyu.user@gmail.com>
2025-04-23 17:30:46 +0900

    fix: modal에 필요한 interface가 union type이라 이에 맞게 수정

--- src/components/modal/MovieModal.tsx
+++ src/components/modal/MovieModal.tsx
@@ -9,8 +9,8 @@ interface MovieModalProps {
   title?: string;
   overview: string;
   name?: string;
-  release_date: string;
-  first_air_date: string;
+  release_date?: string;
+  first_air_date?: string;
   vote_average: number;
   setModalOpen: Dispatch<SetStateAction<boolean>>;
 }
