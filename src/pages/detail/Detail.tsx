204f1fdbbe9aaf6b3b78e73a4baf2d58d1837db1
Shin-Yu-1 <shingyu.user@gmail.com>
2025-04-24 13:15:21 +0900

    refactor: jsx에서 tsx로 변경 및 DetailPage에 MediaItem 타입 추가

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -1,17 +1,36 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { MediaItem } from '@/types/api';
 
 import axios from '@/api/axios';
 
 const DetailPage = () => {
   const { movieId } = useParams();
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState<MediaItem>({
+    id: 0,
+    adult: false,
+    backdrop_path: '',
+    genre_ids: [],
+    original_language: '',
+    overview: '',
+    popularity: 0,
+    poster_path: '',
+    vote_average: 0,
+    vote_count: 0,
+    media_type: 'movie',
+    title: '',
+    original_title: '',
+    release_date: '',
+    video: false,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
       const request = await axios.get(`/movie/${movieId}`);
       setMovie(request.data);
     };
+
+    fetchData();
   }, [movieId]);
 
   if (!movie) return <div>...loading</div>;
