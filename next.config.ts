c0b45669e2461cbb5cb25a524c8a233fec51423f
Shin-Yu-1 <shingyu.user@gmail.com>
2025-05-30 09:19:32 +0900

    fix: config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  swcMinify: true,
   experimental: {
     typedRoutes: true,
   },
